49779df1a204853d585858d2cda24d11aa43293b
joddes <30724194+joddes@users.noreply.github.com>
2017-09-21 16:13:13 +0200

    Datenbank Integration

--- app/src/main/java/com/example/janda/photorun/Login/LoginActivity.java
+++ app/src/main/java/com/example/janda/photorun/Login/LoginActivity.java
@@ -5,4 +5,5 @@ package com.example.janda.photorun.Login;
  */
 
 public class LoginActivity {
+
 }
--- app/src/main/java/com/example/janda/photorun/MainActivity.java
+++ app/src/main/java/com/example/janda/photorun/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.janda.photorun;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 }
