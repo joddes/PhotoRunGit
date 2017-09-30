eef42c15cd72abe48ac9c20c3322444e60445e4b
lisarai <31850592+lisarai@users.noreply.github.com>
2017-09-30 16:25:14 +0200

    Revert "Revert "Erweiterung für Create Photorun""
    This reverts commit 74abe0cdde0ec77bc4ded9867626005bfafc0fc6.

--- app/src/main/java/com/example/janda/photorun/models/photorun_settings.java
+++ app/src/main/java/com/example/janda/photorun/models/photorun_settings.java
@@ -1,18 +1,20 @@
 package com.example.janda.photorun.models;
 
-import java.util.List;
-
 /**
  * Created by user on 25.09.17.
  */
 
 public class photorun_settings {
 
-    private List participators;
+    private boolean participators;
     private String status;
 
-    public photorun_settings(List participators, String status) {
+    public photorun_settings(boolean participators, String status) {
         this.participators = participators;
+        this.status = status;
+    }
+
+    public photorun_settings(String status) {
         this.status = status;
     }
 
@@ -20,11 +22,11 @@ public class photorun_settings {
 
     }
 
-    public List getParticipators() {
+    public boolean getParticipators() {
         return participators;
     }
 
-    public void setParticipators(List participators) {
+    public void setParticipators(boolean participators) {
         this.participators = participators;
     }
 
