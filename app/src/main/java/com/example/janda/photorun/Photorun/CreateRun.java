7a93401eb47cdda10de2832801a60c13331982da
onthewalnuttree <31659221+onthewalnuttree@users.noreply.github.com>
2017-10-09 17:41:11 +0200

    Time Picker implementation

--- app/src/main/java/com/example/janda/photorun/Photorun/CreateRun.java
+++ app/src/main/java/com/example/janda/photorun/Photorun/CreateRun.java
@@ -3,6 +3,7 @@ package com.example.janda.photorun.Photorun;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -14,7 +15,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.janda.photorun.Login.ProfileActivity;
+import com.example.janda.photorun.Helpers.TimePickerFragment;
 import com.example.janda.photorun.R;
 import com.example.janda.photorun.models.Photorun;
 import com.google.firebase.auth.FirebaseAuth;
@@ -29,7 +30,7 @@ public class CreateRun extends AppCompatActivity implements View.OnClickListener
     private Button backButton;
 
 
-    private EditText TitleEditText, DateEditText, Start_timeEditText, Estimated_durationEditText, Start_pointEditText, End_pointEditText ,Max_participatorsEditText, DescriptionEditText;
+    EditText TitleEditText, DateEditText, Start_timeEditText, Estimated_durationEditText, Start_pointEditText, End_pointEditText ,Max_participatorsEditText, DescriptionEditText;
     private TextView toolbar_Textview;
 
     private DatabaseReference mDatabaseRefrence;
@@ -58,6 +59,8 @@ public class CreateRun extends AppCompatActivity implements View.OnClickListener
         Start_pointEditText = (EditText) findViewById(R.id.start_point);
         End_pointEditText = (EditText) findViewById(R.id.end_point);
         Start_timeEditText= (EditText) findViewById(R.id.starting_time);
+        Start_timeEditText.setOnClickListener(this);
+
         Estimated_durationEditText = (EditText) findViewById(R.id.estimated_duration);
         DescriptionEditText = (EditText) findViewById(R.id.description);
 
@@ -215,6 +218,9 @@ public class CreateRun extends AppCompatActivity implements View.OnClickListener
         if(view == submitButton){
             createRun();
         }
+        if (view == Start_timeEditText){
+            showTimePickerDialog(Start_timeEditText);
+        }
 
     }
     @Override
@@ -224,5 +230,9 @@ public class CreateRun extends AppCompatActivity implements View.OnClickListener
         startActivity(new Intent(this, ViewPhotorunList.class));
     }
 
+    public void showTimePickerDialog(View v){
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
 
 }
