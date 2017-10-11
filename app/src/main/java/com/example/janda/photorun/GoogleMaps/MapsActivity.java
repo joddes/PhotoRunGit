d41b0fd0c97ae77d04ac678110fb87ea2db6ca0b
niklashamp <31850317+niklashamp@users.noreply.github.com>
2017-10-11 15:26:37 +0200

    Map changes

--- app/src/main/java/com/example/janda/photorun/GoogleMaps/MapsActivity.java
+++ app/src/main/java/com/example/janda/photorun/GoogleMaps/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.janda.photorun.GoogleMaps;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -19,10 +20,13 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.io.IOException;
 import java.util.List;
@@ -48,14 +52,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        Marker currentLocation;
 
         Intent intent = getIntent();
         startpoint = intent.getStringExtra(ViewSinglePhotoRun.PHOTORUN_STARTPOINT);
         title = intent.getStringExtra(ViewSinglePhotoRun.PHOTORUN_TITLE);
-
-
-
-
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
@@ -136,10 +137,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             Address address = addressList.get(0);
             LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-            locationMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Endpoint: " +title));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(locationMarker.getPosition(), 15));
-
-
+            //locationMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Endpoint: " +title));
+            MarkerOptions locationMarker1 = new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).position(latLng).title("Endpoint: " +title);
+            mMap.addMarker(locationMarker1);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(locationMarker1.getPosition(), 15));
         }
 
 
